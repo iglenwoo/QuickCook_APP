8c31b622198ef861d3285e9167e8e059523c1e03
Ingyu Woo <iglenwoo@gmail.com>
2019-01-09 23:23:16 -0800

    create an ios project

---
+++ QuickCook_APP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuickCook_APP
+//
+//  Created by Ingyu Woo on 1/9/19.
+//  Copyright © 2019 Ingyu Woo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
