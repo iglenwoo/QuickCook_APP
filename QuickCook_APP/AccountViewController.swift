e0e6f7352c467f0d41bea48608bfcfc5b53b844e
Ingyu Woo <iglenwoo@gmail.com>
2019-01-22 15:33:43 -0800

    add Views

---
+++ QuickCook_APP/AccountViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  QuickCook_APP
+//
+//  Created by Ingyu Woo on 1/9/19.
+//  Copyright © 2019 Ingyu Woo. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class AccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+}
--- QuickCook_APP/MainTabBarController.swift
+++ QuickCook_APP/MainTabBarController.swift
@@ -1,8 +1,8 @@
 //
-//  ViewController.swift
+//  MainTabBarController.swift
 //  QuickCook_APP
 //
-//  Created by Ingyu Woo on 1/9/19.
+//  Created by Ingyu Woo on 1/22/19.
 //  Copyright © 2019 Ingyu Woo. All rights reserved.
 //
 
@@ -10,22 +10,22 @@ import UIKit
 import Firebase
 import FirebaseUI
 
-class MainViewController: UIViewController {
+class MainTabBarController: UITabBarController {
 
     /** @var handle
      @brief The handle for the auth state listener, to allow cancelling later.
      */
     var handle: AuthStateDidChangeListenerHandle?
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // NOTE: temporary signout...
-        try! Auth.auth().signOut()
+        //try! Auth.auth().signOut()
     }
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        
+
         // TODO: handle user session correctly
         if Auth.auth().currentUser != nil {
             print("Current user is \"\(Auth.auth().currentUser.debugDescription)\"")
@@ -33,26 +33,26 @@ class MainViewController: UIViewController {
             openLoginView()
         }
     }
-    
+
     /* Listen for authentication state
      Attach a listener to the FIRAuth object.
      This listener gets called whenever the users' sign-in state changes
      */
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
+
         handle = Auth.auth().addStateDidChangeListener { (auth, user) in
             print(user ?? "addStateDidChangeListener called");
         }
     }
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
+
         Auth.auth().removeStateDidChangeListener(handle!)
         print("removeStateDidChangeListener called")
     }
-    
-    
+
+
     @IBAction func signOutTapped(_ sender: UIButton) {
         do {
             try Auth.auth().signOut()
@@ -61,29 +61,29 @@ class MainViewController: UIViewController {
         } catch let signOutError as NSError {
             print("Error signing out: \(signOutError.localizedDescription)")
         }
-        
+
     }
 }
 
-extension MainViewController: FUIAuthDelegate {
+extension MainTabBarController: FUIAuthDelegate {
     func authUI(_ authUI: FUIAuth, didSignInWith authDataResult: AuthDataResult?, error: Error?) {
         if error != nil {
             print(error?.localizedDescription ?? "error?")
             return
         }
-        
+
         let pid = authDataResult!.user.providerID
         print("providerID is \"\(pid)\"")
     }
 }
 
-extension MainViewController {
+extension MainTabBarController {
     func openLoginView() {
         let authUI = FUIAuth.defaultAuthUI()
         authUI?.delegate = self
         let providers: [FUIAuthProvider] = [
             FUIGoogleAuth()]
-        
+
         authUI?.providers = providers
         let authViewController = authUI!.authViewController()
         self.present(authViewController, animated: true, completion: nil)
